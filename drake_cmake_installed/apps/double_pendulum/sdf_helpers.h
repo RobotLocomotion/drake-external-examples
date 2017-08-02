65bd0f3569d201f9766e169f3123816672123f31
Daniel Stonier <d.stonier@gmail.com>
2017-08-01 21:42:35 -0400

    update against recent changes

--- drake_cmake_installed/apps/double_pendulum/sdf_helpers.h
+++ drake_cmake_installed/apps/double_pendulum/sdf_helpers.h
@@ -5,8 +5,8 @@
 #include "drake/multibody/rigid_body_tree.h"
 
 namespace drake {
-namespace examples {
-namespace double_pendulum {
+    namespace examples {
+        namespace double_pendulum {
 
 /// Parses a model from an SDF file into a RigidBodyTree.
 /// @remarks if more than one model is described on the given
@@ -15,10 +15,10 @@ namespace double_pendulum {
 /// @param[in] sdf_path where the model description is.
 /// @param[in,out] tree mutable rigid body tree to parse into.
 /// @return instance id for the newly added model
-int ParseModelFromFile(const std::string& sdf_path,
-                        RigidBodyTree<double>* tree);
+            int ParseModelFromFile(const std::string& sdf_path,
+                                   RigidBodyTree<double>* tree);
 
-}  // namespace double_pendulum
-}  // namespace examples
+        }  // namespace double_pendulum
+    }  // namespace examples
 }  // namespace drake
 
