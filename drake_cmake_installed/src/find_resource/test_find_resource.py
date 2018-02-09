28f716c98b082f210b9796cb653828af6e496f2e
Eric Cousineau <eric.cousineau@tri.global>
2018-02-08 20:07:05 -0500

    Add downstream Python test.

---
+++ drake_cmake_installed/src/find_resource/test_find_resource.py
@@ -0,0 +1,8 @@
+# -*- mode: python -*-
+# vi: set ft= python :
+
+import os
+
+from pydrake.common import FindResourceOrThrow
+
+FindResourceOrThrow('drake/examples/atlas/urdf/atlas_convex_hull.urdf')
