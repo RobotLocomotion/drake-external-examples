0125ad1f71c2d05952b98cb0117928bf148c516e
Jamie Snape <jamie.snape@kitware.com>
2018-02-19 14:07:47 -0500

    Fix pylint issues

--- drake_cmake_installed/src/find_resource/test_find_resource.py
+++ drake_cmake_installed/src/find_resource/test_find_resource.py
@@ -30,7 +30,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-import os
+"""Test the Drake find resource API."""
 
 from pydrake.common import FindResourceOrThrow
 
