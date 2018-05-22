4ae6894f4932bd2bae5ac05e9180dbd0a2c6c52a
Eric Cousineau <eric.cousineau@tri.global>
2018-05-22 12:51:19 -0400

    bazel: Add example consuming pydrake

--- drake_bazel_external/apps/py_binary_test.sh
+++ drake_bazel_external/apps/py_binary_test.sh
@@ -1,7 +1,6 @@
-# -*- mode: python -*-
-# vi: set ft=python :
+#!/bin/bash
 
-# Copyright (c) 2018, Toyota Research Institute.
+# Copyright (c) 2017, Massachusetts Institute of Technology.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,37 +29,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-# Compile a sample application.
-cc_binary(
-    name = "simple_continuous_time_system",
-    srcs = ["simple_continuous_time_system.cc"],
-    deps = [
-        "@drake//systems/analysis",
-        "@drake//systems/framework",
-    ],
-)
-
-# Make a simple Python application.
-py_binary(
-    name = "simple_logging_example",
-    srcs = ["simple_logging_example.py"],
-    deps = [
-        "@drake//bindings/pydrake",
-    ],
-)
-
-# Run the applications to make sure they complete successfully.
-# N.B. For actual development, you should prefer `*_test` targets.
-sh_test(
-    name = "simple_continuous_time_system_test",
-    srcs = [":simple_continuous_time_system"],
-    size = "small",
-)
-
-sh_test(
-    name = "simple_logging_example_test",
-    srcs = ["py_binary_test.sh"],
-    args = ["$(location :simple_logging_example)"],
-    data = [":simple_logging_example"],
-    size = "small",
-)
+# Since `sh_test` cannot consume a `py_binary`, we must pass it to a proxy
+# script.
+exec "$@"
