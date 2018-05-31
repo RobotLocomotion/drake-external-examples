798d43f3c4fb55a17d8a7fd88aea639fb67e0ac0
Eric Cousineau <eric.cousineau@tri.global>
2018-05-30 20:18:59 -0400

    bazel: Add example of custom bindings using drake + pydrake

---
+++ drake_bazel_external/apps/simple_adder.h
@@ -0,0 +1,59 @@
+/*****************************************************************************
+ * Copyright (c) 2018, Toyota Research Institute
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+
+/**
+ * @file
+ * Provides an example of creating a simple Drake C++ system that will later be
+ * bound.
+ *
+ * This is the header portion of the -inl.h pattern:
+ * http://drake.mit.edu/cxx_inl.html#cxx-inl-files
+ */
+
+#include <drake/systems/framework/leaf_system.h>
+
+namespace shambhala {
+
+/// Adds a constant to an input.
+template <typename T>
+class SimpleAdder : public drake::systems::LeafSystem<T> {
+ public:
+  explicit SimpleAdder(T add);
+
+ private:
+  void CalcOutput(
+      const drake::systems::Context<T>& context,
+      drake::systems::BasicVector<T>* output) const;
+
+  const T add_{};
+};
+
+}  // namespace shambhala
---
+++ drake_bazel_external/apps/simple_adder_test.cc
@@ -0,0 +1,82 @@
+/*****************************************************************************
+ * Copyright (c) 2018, Toyota Research Institute
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+
+/**
+ * @file
+ * Provides an example of testing a custom C++ Drake system which will also
+ * be bound in Python.
+ */
+
+#include <iostream>
+
+#include <drake/common/text_logging.h>
+#include <drake/systems/analysis/simulator.h>
+#include <drake/systems/framework/diagram_builder.h>
+#include <drake/systems/primitives/constant_vector_source.h>
+#include <drake/systems/primitives/signal_logger.h>
+
+#include "simple_adder.h"
+
+using drake::systems::Simulator;
+using drake::systems::DiagramBuilder;
+using drake::systems::ConstantVectorSource;
+using drake::systems::SignalLogger;
+
+namespace shambhala {
+namespace {
+
+int DoMain() {
+  DiagramBuilder<double> builder;
+  auto source = builder.AddSystem<ConstantVectorSource<double>>(
+      Eigen::VectorXd::Constant(1, 10.));
+  auto adder = builder.AddSystem<SimpleAdder<double>>(100.);
+  builder.Connect(source->get_output_port(), adder->get_input_port(0));
+  auto logger = builder.AddSystem<SignalLogger<double>>(1);
+  builder.Connect(adder->get_output_port(0), logger->get_input_port());
+  auto diagram = builder.Build();
+
+  Simulator<double> simulator(*diagram);
+  simulator.StepTo(1);
+
+  auto x = logger->data();
+  Eigen::VectorXd x_expected = Eigen::Vector2d(110., 110.);
+  std::cout << "Output values: " << x << std::endl;
+  DRAKE_DEMAND(x.isApprox(x_expected));
+
+  return 0;
+}
+
+}  // namespace
+}  // namespace shambhala
+
+int main() {
+  return shambhala::DoMain();
+}
