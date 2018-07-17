ed4f9ceb71c8bf128f47e0a6543deb38d0025260
Eric Cousineau <eric.cousineau@tri.global>
2018-07-17 16:06:05 -0400

    Hotfix for rename of input_port{_descriptor => }.h

--- drake_cmake_installed/src/particles/particle_test.cc
+++ drake_cmake_installed/src/particles/particle_test.cc
@@ -35,7 +35,7 @@
 
 #include <gtest/gtest.h>
 
-#include <drake/systems/framework/input_port_descriptor.h>
+#include <drake/systems/framework/input_port.h>
 #include <drake/systems/framework/output_port_value.h>
 #include <drake/systems/framework/system.h>
 #include <drake/systems/framework/vector_base.h>
@@ -97,10 +97,10 @@ TYPED_TEST_P(ParticleTest, OutputTest) {
 /// consistent with its state and input (velocity and acceleration).
 TYPED_TEST_P(ParticleTest, DerivativesTest) {
   // Set input.
-  const drake::systems::InputPortDescriptor<TypeParam>& input_descriptor =
+  const drake::systems::InputPort<TypeParam>& input_port =
       this->dut_->get_input_port(0);
   auto input = std::make_unique<drake::systems::BasicVector<TypeParam>>(
-      input_descriptor.size());
+      input_port.size());
   input->SetZero();
   input->SetAtIndex(0, static_cast<TypeParam>(1.0));  // u0 = 1 m/s^2
   this->context_->FixInputPort(0, std::move(input));
