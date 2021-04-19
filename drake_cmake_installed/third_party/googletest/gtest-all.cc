059f47a10420067e39ead3cec13e20908f6fa86d
Jamie Snape <jamie.snape@kitware.com>
2021-04-19 11:27:07 -0400

    Change default branch to main and expunge most usage of term master

--- drake_cmake_installed/third_party/googletest/gtest-all.cc
+++ drake_cmake_installed/third_party/googletest/gtest-all.cc
@@ -6968,7 +6968,7 @@ bool ShouldRunTestOnShard(int total_shards, int shard_index, int test_id) {
 // each TestSuite and TestInfo object.
 // If shard_tests == true, further filters tests based on sharding
 // variables in the environment - see
-// https://github.com/google/googletest/blob/master/googletest/docs/advanced.md
+// https://github.com/google/googletest/blob/HEAD/docs/advanced.md
 // . Returns the number of tests that should run.
 int UnitTestImpl::FilterTests(ReactionToSharding shard_tests) {
   const Int32 total_shards = shard_tests == HONOR_SHARDING_PROTOCOL ?
@@ -7899,8 +7899,8 @@ static std::string DeathTestThreadWarning(size_t thread_count) {
     msg << "detected " << thread_count << " threads.";
   }
   msg << " See "
-         "https://github.com/google/googletest/blob/master/googletest/docs/"
-         "advanced.md#death-tests-and-threads"
+         "https://github.com/google/googletest/blob/HEAD/docs/advanced.md"
+         "#death-tests-and-threads"
       << " for more explanation and suggested solutions, especially if"
       << " this is the last message you see before your test times out.";
   return msg.GetString();
